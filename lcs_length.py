34ffad56014e3de4730deff220dccb8bc132675b
maxwellsarpong <maxwellsarpong07@gmail.com>
2022-03-23 15:15:57 +0000

    added an import for utf-8 coding for the symbols, and restructured the comments in the code

--- lcs_length.py
+++ lcs_length.py
@@ -1,19 +1,23 @@
-# The longest common subsequence in Python
-from tabulate import tabulate
+# coding: utf-8
+'''
+    Finding the LCS_length().
 
+    i.find the length of the two strings
+    ii.declare an array for storing the dp values
+    iii.build L[m + 1][n + 1] in bottom up fashion
+        Note:
+            L[i][j] contains length of LCS of X[0..i-1]
+            and Y[0..j-1]
+'''
 
 def lcs_length(X, Y):
-    # find the length of the strings
+    
     m = len(X)
     n = len(Y)
   
-    # declaring the array for storing the dp values
     L = [[0]*(n + 1) for i in range(m + 1)]
     B = [[0]*(n + 1) for i in range(m + 1)]
   
-    """Following steps build L[m + 1][n + 1] in bottom up fashion
-    Note: L[i][j] contains length of LCS of X[0..i-1]
-    and Y[0..j-1]"""
     for i in range(m + 1):
         for j in range(n + 1):
             if i == 0 or j == 0 :
@@ -28,6 +32,4 @@ def lcs_length(X, Y):
                 L[i][j] = L[i][j-1]
                 B[i][j] = '←'
                 
-    # print(tabulate(L))
-    # print(tabulate(B))
     return L, B
