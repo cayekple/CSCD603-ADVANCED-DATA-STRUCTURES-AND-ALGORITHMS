a3d4fe3787d6003397dd9de1d680f9c0909e8c3f
Clemence Ayekple <ayekpleclemence@gmail.com>
2022-03-30 18:12:31 +0000

    feat: Q2 updated

--- Q2.py
+++ Q2.py
@@ -1,11 +1,12 @@
 def CSCD(s, i, j):
+    
     if i == j:
-        print ("A")
+        print ("A"+str(i))
     else:
         print("(")
-        CSCD(s, i, s[i][j])
-        CSCD(s, s[i][j], j)
+        CSCD(s, i, s[i-1][j-1])
+        CSCD(s, s[i-1][j-1] + 1, j)
         print(")")
 
-s = []
-CSCD(s, 1,6)
+s = [[0,1,2,1], [0,0,2,2], [0,0,0,3], [0,0,0,0]]
+CSCD(s, 1, 4)
